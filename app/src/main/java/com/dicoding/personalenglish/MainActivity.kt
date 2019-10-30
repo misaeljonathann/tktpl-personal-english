f2ad8b0c1a5a5e7dfbfab4e9f58a86cd12ca28ba
Misael Jonathan <misaeljonathan17@gmail.com>
2019-10-30 11:24:52 +0700

    progress on homepage and navigation bar

--- app/src/main/java/com/dicoding/personalenglish/MainActivity.kt
+++ app/src/main/java/com/dicoding/personalenglish/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.dicoding.personalenglish
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.dicoding.personalenglish.ui.WordViewModel
-import com.dicoding.personalenglish.util.InjectorUtils
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var wordViewModel: WordViewModel
-    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navbar: BottomNavigationView = findViewById(R.id.navigation)
+
+        navbar.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    goToHomePage()
+                }
+
+                R.id.navigation_challenge -> {
+                    goToChallengesPage()
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+
+    fun goToHomePage(): Boolean {
+        val fragment = HomepageFragment() //GANTI INI NANTI
+        val fragmentTransaction: FragmentTransaction = this.supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_home, fragment, fragment.toString())
+        fragmentTransaction.addToBackStack(fragment.toString())
+        fragmentTransaction.commit()
+        return true
+    }
+
+    fun goToChallengesPage(): Boolean {
+        val fragment = HistoryItemFragment() //GANTI INI NANTI
+        val fragmentTransaction: FragmentTransaction = this.supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_home, fragment, fragment.toString())
+        fragmentTransaction.addToBackStack(fragment.toString())
+        fragmentTransaction.commit()
+        return true
     }
 }
